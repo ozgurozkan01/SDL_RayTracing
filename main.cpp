6c7544eda732e62edce296fd429f3fff0f421602
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-10 03:07:27 +0300

    add SDL2 lib and create window succesfully

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    /*
+    * Initialises the SDL video subsystem (as well as the events subsystem).
+    * Returns 0 on success or a negative error code on failure using SDL_GetError().
+    */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /* Creates a SDL window */
+    SDL_Window *window = SDL_CreateWindow("SDL Example", /* Title of the SDL window */
+                              SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                              SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                              400, /* Width of the window in pixels */
+                              300, /* Height of the window in pixels */
+                              0); /* Additional flag(s) */
+
+    /* Checks if window has been created; if not, exits program */
+    if (window == nullptr)
+    {
+        fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /* Frees memory */
+    SDL_DestroyWindow(window);
+
+    /* Shuts down all SDL subsystems */
+    SDL_Quit();
+
     return 0;
 }
