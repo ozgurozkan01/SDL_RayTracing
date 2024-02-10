5b64f4a13ed3e3bd04aa3c68b0ea160c7d42b73d
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-10 22:54:47 +0300

    add image class to control what is going to be drawn on the screen

--- Image.cpp
+++ Image.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "Image.h"
+#include <iostream>
+
+Image::Image(int width, int height, SDL_Renderer* renderer) :
+    window_width(width),
+    window_height(height),
+    renderer(renderer)
+{
+    init();
+    std::cout << newVec.x << ", " << newVec.y << std::endl;
+}
+
+Image::~Image()
+{
+    delete [] imagePixels;
+    SDL_DestroyTexture(texture);
+}
+
+void Image::init()
+{
+    imagePixels = new uint32_t[window_width * window_height];
+    texture = SDL_CreateTexture(renderer,
+                                SDL_PIXELFORMAT_RGBA8888,
+                                SDL_TEXTUREACCESS_STATIC,
+                                window_width,
+                                window_height);
+}
+
+uint32_t Image::getPixelColor(glm::vec2 coords) const
+{
+    uint8_t red = (coords.x) / (window_width-1) * 255;
+    uint8_t green = (double(coords.y) / (window_height-1)) * 255;
+
+    uint32_t pixelColor = 0x00000000;
+    pixelColor += red;
+    pixelColor <<= 8;
+    pixelColor += green;
+    pixelColor <<= 8;
+    pixelColor += 0x00;
+    pixelColor <<= 8;
+    pixelColor += 0xFF;
+
+    return pixelColor;
+}
+
+void Image::setPixelColor()
+{
+    for (int y = 0; y < window_height; ++y)
+    {
+        for (int x = 0; x < window_width; ++x)
+        {
+            glm::vec2 pixelCoord(x, y);
+            imagePixels[x + y * window_width] = getPixelColor(pixelCoord);
+        }
+    }
+}
+
+void Image::display()
+{
+    setPixelColor();
+    SDL_UpdateTexture(texture, nullptr, imagePixels, window_width * sizeof(uint32_t));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
+}
--- Image.h
+++ Image.h
@@ -5,9 +5,29 @@
 #ifndef SDL_RAYTRACING_IMAGE_H
 #define SDL_RAYTRACING_IMAGE_H
 
+#include <cstdint>
+#include <SDL2/SDL.h>
+#include <GLM/glm/glm.hpp>
 
 class Image {
+public:
+    Image(int width, int height, SDL_Renderer* renderer);
+    ~Image();
 
+    void init();
+    [[nodiscard]] uint32_t getPixelColor(glm::vec2 coords) const;
+    void setPixelColor();
+    void display();
+
+private:
+    SDL_Texture* texture;
+    SDL_Renderer* renderer;
+    uint32_t * imagePixels;
+
+    glm::vec3 newVec;
+
+    const int window_width;
+    const int window_height;
 };
 
 
