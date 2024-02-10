2e48d456feccd2a00e39f9275c5d46eacb833fb7
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-10 03:09:58 +0300

    add render class to control view of screen such as pixels etc

---
+++ Render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 10/02/2024.
+//
+
+#include "Render.h"
---
+++ Render.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 10/02/2024.
+//
+
+#ifndef SDL_RAYTRACING_RENDER_H
+#define SDL_RAYTRACING_RENDER_H
+
+
+class Render {
+
+};
+
+
+#endif //SDL_RAYTRACING_RENDER_H
