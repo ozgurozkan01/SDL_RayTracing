041470eacc7d2a3b777193cedd0edad94b4b4bbf
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-10 03:08:42 +0300

    add screen class to control window behaviours

---
+++ Screen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 10/02/2024.
+//
+
+#include "Screen.h"
---
+++ Screen.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 10/02/2024.
+//
+
+#ifndef SDL_RAYTRACING_SCREEN_H
+#define SDL_RAYTRACING_SCREEN_H
+
+
+class Screen {
+
+};
+
+
+#endif //SDL_RAYTRACING_SCREEN_H
