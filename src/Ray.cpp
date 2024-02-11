bfb0842c4a87164041748e311d720eab1fb8b2f9
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-11 23:53:52 +0300

    add ray class

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Ray.h"
+
+Ray::Ray(const glm::vec2 point1, const glm::vec2 point2)
+{
+    this->point1 = point1;
+    this->point2 = point2;
+    pointsDiff = point2 - point1;
+}
--- src/Ray.h
+++ src/Ray.h
@@ -5,9 +5,21 @@
 #ifndef SDL_RAYTRACING_RAY_H
 #define SDL_RAYTRACING_RAY_H
 
+#include <GLM/glm/glm.hpp>
 
-class Ray {
+class Ray
+{
+public:
+    Ray(const glm::vec2 point1, const glm::vec2 point2);
 
+    [[nodiscard]] inline glm::vec2 getPoint1() const { return point1; }
+    [[nodiscard]] inline glm::vec2 getPoint2() const { return point2; }
+
+private:
+
+    glm::vec2 point1;
+    glm::vec2 point2;
+    glm::vec2 pointsDiff;
 };
 
 
