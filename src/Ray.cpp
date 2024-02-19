74a84f1b3c7918348b425384c6d1273452548445
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-19 21:53:32 +0300

    add ray class to control ray functions

---
+++ src/Ray.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ozgur on 18/02/2024.
+//
+
+#include "Ray.h"
+#include "Sphere.h"
+
+const double infinity = std::numeric_limits<double>::infinity();
+
+Ray::Ray(const glm::vec3 &_origin, const glm::vec3 &_direction):
+    origin(_origin),
+    direction(_direction) {}
+
+glm::vec3 Ray::getOrigin() const {
+    return origin;
+}
+
+glm::vec3 Ray::getDirection() const {
+    return direction;
+}
+
+glm::vec3 Ray::at(float t) const {
+    return origin + (t * direction);
+}
+
+glm::vec3 Ray::rayColor(const Ray &ray, const Sphere& sphere)
+{
+    HitInfo hitInfo;
+    if (sphere.isHit(ray, 0, infinity, hitInfo))
+    {
+        return 0.5f * (hitInfo.normal + glm::vec3(1,1,1));
+    }
+
+    glm::vec3 unitDirection = glm::normalize(ray.getDirection());
+    auto a = 0.5*(unitDirection.y + 1.0);
+    return ((1.0f - (float)a) * glm::vec3(1.0, 1.0, 1.0)) + ((float)a * glm::vec3(0.5, 0.7, 1.0));
+}
---
+++ src/Ray.h
@@ -0,0 +1,28 @@
+//
+// Created by ozgur on 18/02/2024.
+//
+
+#ifndef SDL_RAYTRACING_RAY_H
+#define SDL_RAYTRACING_RAY_H
+
+#include "GLM/glm/glm.hpp"
+
+class Sphere;
+
+class Ray {
+public:
+    Ray() = default;
+    Ray(const glm::vec3& _origin, const glm::vec3& _direction);
+
+    [[nodiscard]] glm::vec3 getOrigin() const;
+    [[nodiscard]] glm::vec3 getDirection() const;
+    [[nodiscard]] glm::vec3 at(float t) const;
+
+    glm::vec3 rayColor(const Ray& ray, const Sphere& sphere);
+private:
+    glm::vec3 origin;
+    glm::vec3 direction;
+};
+
+
+#endif //SDL_RAYTRACING_RAY_H
