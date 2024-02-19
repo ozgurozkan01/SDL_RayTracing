ce155e788f91f3aff0b76d69e07d37295518633e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-19 21:20:49 +0300

    add sphere class

---
+++ src/Sphere.cpp
@@ -0,0 +1,35 @@
+#include "Sphere.h"
+
+Sphere::Sphere(glm::vec3 _center, float _radius)
+        : center(_center), radius(_radius)
+{}
+
+bool Sphere::isHit(const Ray &ray, double ray_tmin, double ray_tmax, HitInfo &hitInfo) const
+{
+    glm::vec3 oc = ray.getOrigin() - center;
+    auto a = glm::dot(ray.getDirection(), ray.getDirection());
+    auto halfB = dot(oc, ray.getDirection());
+    auto c = glm::dot(oc, oc) - radius*radius;
+
+    auto discriminant = halfB * halfB - a * c;
+
+    if (discriminant < 0) return false;
+    auto sqrtd = sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+    auto root = (-halfB - sqrtd) / a;
+    if (root <= ray_tmin || ray_tmax <= root) {
+        root = (-halfB + sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+            return false;
+    }
+
+    hitInfo.t = root;
+    hitInfo.p = ray.at((float)hitInfo.t);
+
+    glm::vec3 outwardNormal = (hitInfo.p - center) / radius;
+    glm::normalize(outwardNormal);
+    hitInfo.setFaceNormal(ray, outwardNormal);
+
+    return true;
+}
---
+++ src/Sphere.h
@@ -0,0 +1,23 @@
+//
+// Created by ozgur on 19/02/2024.
+//
+
+#ifndef SDL_RAYTRACING_SPHERE_H
+#define SDL_RAYTRACING_SPHERE_H
+
+#include "GLM/glm/glm.hpp"
+#include "Hittable.h"
+
+class Sphere : public Hittable{
+public:
+public:
+    Sphere(glm::vec3 _center, float _radius);
+
+    bool isHit(const Ray& ray, double ray_tmin, double ray_tmax, HitInfo& hitInfo) const override;
+private:
+    float radius;
+    glm::vec3 center;
+};
+
+
+#endif //SDL_RAYTRACING_SPHERE_H
