549efd587c129c79c12df9c3366395649d1d55b9
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-27 22:29:53 +0300

    fill Vector3 class

--- src/Vector3.h
+++ src/Vector3.h
@@ -17,7 +17,102 @@ struct Vector3
     [[nodiscard]] double x() const { return coordinates[0]; }
     [[nodiscard]] double y() const { return coordinates[1]; }
     [[nodiscard]] double z() const { return coordinates[2]; }
+
+    Vector3 operator-() const { return Vector3(-coordinates[0], -coordinates[1], -coordinates[2]); }
+    double operator[](int index) const { return coordinates[index]; }
+    double& operator[](int index) { return coordinates[index]; }
+
+    Vector3& operator+=(const Vector3& vector3)
+    {
+        coordinates[0] += vector3.coordinates[0];
+        coordinates[1] += vector3.coordinates[1];
+        coordinates[2] += vector3.coordinates[2];
+
+        return *this;
+    }
+
+    Vector3& operator*=(double t)
+    {
+        coordinates[0] *= t;
+        coordinates[1] *= t;
+        coordinates[2] *= t;
+
+        return *this;
+    }
+
+    Vector3& operator/=(double t)
+    {
+        return *this *= (1/t);
+    }
+
+    double length() const
+    {
+        return sqrt(dotProduct());
+    }
+
+    double dotProduct() const
+    {
+        return coordinates[0] * coordinates[0] +
+                coordinates[1] * coordinates[1] +
+                coordinates[2] * coordinates[2];
+    }
 };
 
+// Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream &out, const Vector3 &v) {
+    return out << v.coordinates[0] << ' ' << v.coordinates[1] << ' ' << v.coordinates[2];
+}
+
+inline Vector3 operator+(const Vector3 &u, const Vector3 &v)
+{
+    return Vector3(u.coordinates[0] + v.coordinates[0],
+                   u.coordinates[1] + v.coordinates[1],
+                   u.coordinates[2] + v.coordinates[2]);
+}
+
+inline Vector3 operator-(const Vector3 &u, const Vector3 &v)
+{
+    return Vector3(u.coordinates[0] - v.coordinates[0],
+                   u.coordinates[1] - v.coordinates[1],
+                   u.coordinates[2] - v.coordinates[2]);
+}
+
+inline Vector3 operator*(const Vector3 &u, const Vector3 &v)
+{
+    return Vector3(u.coordinates[0] * v.coordinates[0],
+                   u.coordinates[1] * v.coordinates[1],
+                   u.coordinates[2] * v.coordinates[2]);
+}
+
+inline Vector3 operator*(double t, const Vector3 &v) {
+    return Vector3(t*v.coordinates[0],
+                   t*v.coordinates[1],
+                   t*v.coordinates[2]);
+}
+
+inline Vector3 operator*(const Vector3 &v, double t) {
+    return t * v;
+}
+
+inline Vector3 operator/(Vector3 v, double t) {
+    return (1/t) * v;
+}
+
+inline double dot(const Vector3 &u, const Vector3 &v) {
+    return u.coordinates[0] * v.coordinates[0]
+           + u.coordinates[1] * v.coordinates[1]
+           + u.coordinates[2] * v.coordinates[2];
+}
+
+inline Vector3 cross(const Vector3 &u, const Vector3 &v) {
+    return Vector3(u.coordinates[1] * v.coordinates[2] - u.coordinates[2] * v.coordinates[1],
+                u.coordinates[2] * v.coordinates[0] - u.coordinates[0] * v.coordinates[2],
+                u.coordinates[0] * v.coordinates[1] - u.coordinates[1] * v.coordinates[0]);
+}
+
+inline Vector3 unit_vector(Vector3 v) {
+    return v / v.length();
+}
 
 #endif //SDL_RAYTRACING_VECTOR3_H
