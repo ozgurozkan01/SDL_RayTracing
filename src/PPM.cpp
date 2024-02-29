747af088d97826f9bb510b1d52e8d84d4036bf1f
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-02-29 19:46:06 +0300

    add PPM class to write whole image into it

---
+++ src/PPM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 29/02/2024.
+//
+
+#include "PPM.h"
---
+++ src/PPM.h
@@ -0,0 +1,19 @@
+//
+// Created by ozgur on 29/02/2024.
+//
+
+#ifndef SDL_RAYTRACING_PPM_H
+#define SDL_RAYTRACING_PPM_H
+
+
+class PPM {
+    int windowWidth;
+    int windowHeight;
+
+    PPM();
+
+
+};
+
+
+#endif //SDL_RAYTRACING_PPM_H
